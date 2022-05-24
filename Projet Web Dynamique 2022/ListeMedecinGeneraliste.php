c1881872c324df46f1e902c62986a891f689f221
walidza1 <93973223+walidza1@users.noreply.github.com>
2022-05-24 17:16:36 +0200

    Add files via upload

---
+++ Projet Web Dynamique 2022/ListeMedecinGeneraliste.php
@@ -0,0 +1,45 @@
+<?php
+  $host = 'localhost';
+  $dbname = 'omnessante2';
+  $username = 'root';
+  $password = 'root';
+    
+  $dsn = "mysql:host=$host;dbname=$dbname"; 
+  $sql = "SELECT Nom,Prenom,Specialite FROM medecin where Specialite LIKE 'Généraliste'";
+   
+  try{
+   $pdo = new PDO($dsn, $username, $password);
+   $stmt = $pdo->query($sql);
+   
+   if($stmt === false){
+    die("Erreur");
+   }
+   
+  }catch (PDOException $e){
+    echo $e->getMessage();
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>Afficher la table medecin</head>
+<body>
+ <h1>Liste des Médecins Géneraliste</h1>
+ <table>
+   <thead>
+     <tr>
+       <th>Nom</th>
+       <th>Prenom</th>
+     </tr>
+   </thead>
+   <tbody>
+     <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+     <tr>
+       <td><a href="PageAffichage.html"><?php echo htmlspecialchars($row['Nom']);?></a></td>
+       <td><?php echo htmlspecialchars($row['Prenom']); ?></td>
+     </tr>
+     <?php endwhile; ?>
+   </tbody>
+ </table>
+</body>
+</html>
